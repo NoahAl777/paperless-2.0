d2bce4bcbda64c3e939f4e94cbb5570aa7bbca8b
Noah <crashlive112@gmail.com>
2022-02-15 21:31:46 -0500

    create PatientList component and import to App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import NavBar from "./Components/NavBar"
+import PatientList from './Components/PatientList';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <PatientList />
     </div>
   );
 }
---
+++ src/Components/PatientList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PatientList = () => {
+  return (
+    <h1>Patient Component</h1>
+  );
+};
+
+export default PatientList;
