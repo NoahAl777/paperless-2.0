6155b5422ccf8ecc9c1b153a31b2556cf81c9686
Noah <crashlive112@gmail.com>
2022-02-15 22:44:34 -0500

    PatientInfo component uses selected patint prop to display patient info

--- src/Components/PatientInfo.js
+++ src/Components/PatientInfo.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const PatientInfo = () => {
+const PatientInfo = ({ selectedPatient }) => {
   return (
     <div className="PatientInfo">
       <h2>PatientInfo Component</h2>
+      <p>{`${selectedPatient.id} | ${selectedPatient.firstName} | ${selectedPatient.lastName}`}</p>
     </div>
   );
 };
