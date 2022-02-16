a75967440f955e562dce501dbb37f8e8edaff875
Noah <crashlive112@gmail.com>
2022-02-15 21:37:22 -0500

    import useState and create patients state in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 import NavBar from "./Components/NavBar"
 import PatientList from './Components/PatientList';
 
 function App() {
+  const [patients, setPatients] = []
+
   return (
     <div className="App">
       <NavBar />
