78129658070d5aa467508277a511e5e8a3fef329
Noah <crashlive112@gmail.com>
2022-02-16 12:23:20 -0500

    create Filter component

---
+++ src/Components/Filter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Filter = () => {
+  return (
+    <div className="Filter">
+      <h2>Filter component</h2>
+    </div>
+  );
+};
+
+export default Filter
--- src/Components/PatientInfo.js
+++ src/Components/PatientInfo.js
@@ -3,7 +3,6 @@ import React from "react";
 const PatientInfo = ({ selectedPatient }) => {
   return (
     <div className="PatientInfo">
-      <h2>PatientInfo Component</h2>
       <p>{`${selectedPatient.id} | ${selectedPatient.firstName} | ${selectedPatient.lastName}`}</p>
     </div>
   );
