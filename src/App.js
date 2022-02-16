80daa9c2c6b389222e7e40728ecff16a8a27882d
Noah <crashlive112@gmail.com>
2022-02-15 21:38:21 -0500

    import useEffect into App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar"
 import PatientList from './Components/PatientList';
 
