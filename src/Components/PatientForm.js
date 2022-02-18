c622a352805227986142d3009902b1f7079ba99c
Noah <crashlive112@gmail.com>
2022-02-18 16:05:36 -0500

    add simple patient form

--- src/Components/PatientForm.js
+++ src/Components/PatientForm.js
@@ -4,6 +4,13 @@ const PatientForm = ({ patientsUrl }) => {
   return (
     <div className="PatientForm">
       <h3>PatientForm Component</h3>
+      <form>
+        <label>First Name</label>
+        <input type="text"></input>
+        <label>Last Name</label>
+        <input type="text"></input>
+        <input type="submit"></input>
+      </form>
     </div>
   );
 };
