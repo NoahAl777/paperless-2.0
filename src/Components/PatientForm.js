307710e8f4547c3f4e49ad04fc124415b8993bc2
Noah <crashlive112@gmail.com>
2022-02-18 16:11:02 -0500

    onSubmit of form prevent rerender with handleSubmit function

--- src/Components/PatientForm.js
+++ src/Components/PatientForm.js
@@ -1,16 +1,22 @@
 import React from "react";
 
 const PatientForm = ({ patientsUrl }) => {
+
+  const handleSubmit = (event) => {
+    console.log(event)
+    event.preventDefault()
+  }
+
   return (
     <div className="PatientForm">
       <h3>PatientForm Component</h3>
-      <form>
+      <form onSubmit={(event) => handleSubmit(event)}>
         <label>First Name</label>
         <input type="text"></input>
         <label>Last Name</label>
         <input type="text"></input>
         <input type="submit"></input>
-      </form>
+      </form >
     </div>
   );
 };
