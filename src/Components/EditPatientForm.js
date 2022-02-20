9d7521be08c94d96bc404017ee678e7df3a0e711
Noah <crashlive112@gmail.com>
2022-02-20 15:35:44 -0500

    create EditPatientForm component

---
+++ src/Components/EditPatientForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditPatientForm = () => {
+  return (
+    <div className="EditPatientForm">
+      <h3>Edit Patient Form</h3>
+    </div>
+  );
+};
+
+export default EditPatientForm;
