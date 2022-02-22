68f665bb868f4574a97e05033ce96d7e7ad53e98
Noah <crashlive112@gmail.com>
2022-02-21 22:15:49 -0500

    add navigation links to navbar with the link component

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="NavBar">
-      <h1>NavBar Component</h1>
+      <Link to="/">Home</Link>
+      <Link to="/patients">Patients</Link>
+      <Link to="/newPatientForm">New Patient</Link>
     </div>
   );
 };
