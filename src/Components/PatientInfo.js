bfe5a3852a2b1d19ed7c030d124a10e629505b40
Noah <crashlive112@gmail.com>
2022-02-21 22:18:56 -0500

    add navigation button for editing form of patient

--- src/Components/PatientInfo.js
+++ src/Components/PatientInfo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PatientInfo = ({ selectedPatient, patients, setPatients, setSelectedPatient, patientsUrl }) => {
 
@@ -14,6 +15,9 @@ const PatientInfo = ({ selectedPatient, patients, setPatients, setSelectedPatien
     <>
       <p>{`${selectedPatient.id} | ${selectedPatient.firstName} | ${selectedPatient.lastName}`}</p>
       <button onClick={() => handleDelete(selectedPatient)}>x</button>
+      <Link to="/edit">
+        <button>Edit</button>
+      </Link>
     </>
   );
 };
