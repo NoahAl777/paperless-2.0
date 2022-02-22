4bb33335d5ec53231bffcd75ac824ad146739342
Noah <crashlive112@gmail.com>
2022-02-22 01:28:16 -0500

    reverting to previous version

--- src/Components/PatientInfo.js
+++ src/Components/PatientInfo.js
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-import React, { useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
-=======
 import React from "react";
->>>>>>> parent of bfe5a38 (add navigation button for editing form of patient)
 
 const PatientInfo = ({ selectedPatient, patients, setPatients, setSelectedPatient, patientsUrl }) => {
-  const params = useParams()
-  const patientCard = []
 
   useEffect(() => {
     setSelectedPatient(patients.filter((patient) => patient.id === parseInt(params.id)))
@@ -22,29 +15,12 @@ const PatientInfo = ({ selectedPatient, patients, setPatients, setSelectedPatien
       .then(setSelectedPatient(patients[0]))
   }
 
-<<<<<<< HEAD
-  if (Object.keys(selectedPatient).length === 0) {
-    return <p>Loading...</p>
-  } else {
-    return (
-      <>
-        <p>{`${selectedPatient[0].id} | ${selectedPatient[0].firstName} | ${selectedPatient[0].lastName}`}</p>
-        <button onClick={() => handleDelete(selectedPatient)}>x</button>
-        <Link to="/edit">
-          <button>Edit</button>
-        </Link>
-      </>
-    )
-  }
-
-=======
   return (
     <>
       <p>{`${selectedPatient.id} | ${selectedPatient.firstName} | ${selectedPatient.lastName}`}</p>
       <button onClick={() => handleDelete(selectedPatient)}>x</button>
     </>
   );
->>>>>>> parent of bfe5a38 (add navigation button for editing form of patient)
 };
 
 export default PatientInfo;
